5048e9f4f2bd9bd71795c27f1916803e0945264c
ksenlisan <101488776+ksenlisan@users.noreply.github.com>
2022-09-06 15:33:42 -0700

    Testing flask app deployment

---
+++ flask_test.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__main__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the mane page <h1>HELLO<h1>"
+
+if __name__ == "__main__":
+    app.run()
