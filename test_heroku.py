7c14a3215135bd4f21181c1dcd801f7aef5f8396
Ksenia Lisova <101488776+ksenlisan@users.noreply.github.com>
2022-09-07 08:59:24 -0700

    Update test_heroku.py

--- test_heroku.py
+++ test_heroku.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the mane page <h1>HELLO<h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    app.run()
