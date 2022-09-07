14ab3435641eee8c4858091ff3d01a4dee3740b6
ksenlisan <101488776+ksenlisan@users.noreply.github.com>
2022-09-07 16:26:21 -0700

    Cleaned up the analysis file, made structiral improvements. Created files containing pre-processed crime locations and edge weights for the whole area.

--- flask_test.py
+++ flask_test.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import folium
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the mane page <h1>HELLO<h1>"
+    map = folium.Map(location=[34.09,-118.36], zoom_start=14)
+    return map._repr_html_()
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
 
 if __name__ == "__main__":
     app.run()
