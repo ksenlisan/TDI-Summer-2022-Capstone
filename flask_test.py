ce7f2ddddf379908b9125e2229f3b1465def4cee
ksenlisan <101488776+ksenlisan@users.noreply.github.com>
2022-09-07 16:55:07 -0700

    Adding html template to test

--- flask_test.py
+++ flask_test.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 import folium
 
 app = Flask(__name__)
+app.secret_key = b'cjgfkgeer747^^&%*R'
 
 @app.route("/")
 def home():
     map = folium.Map(location=[34.09,-118.36], zoom_start=14)
     return map._repr_html_()
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
+@app.route("/main")
+def index():
+    flash("Say something")
+    return render_template("html_template.html") 
 
 if __name__ == "__main__":
     app.run()
